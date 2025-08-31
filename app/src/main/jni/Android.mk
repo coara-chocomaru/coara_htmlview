90dc7f7f2b2fa30a6b5bd42921ee8b92d6527808
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-09-01 08:41:12 +0900

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE           := proc
-LOCAL_SRC_FILES        := proc.c
-LOCAL_LDLIBS           := -llog
+LOCAL_MODULE           := highlighter
+LOCAL_SRC_FILES        := highlighter.c
+LOCAL_LDLIBS           := -llog -lpcre
 include $(BUILD_SHARED_LIBRARY)
