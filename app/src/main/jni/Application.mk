58a2b2fa28ed53a8113374876482cbdf43c9a7d0
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-09-01 10:34:00 +0900

    Update Application.mk

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,12 +1,4 @@
-APP_CFLAGS     := -Wall -Wextra -Wmost
-APP_CFLAGS     += -fno-stack-protector -fomit-frame-pointer
-APP_CONLYFLAGS := -std=c2x
-APP_LDFLAGS    := -Wl,--gc-sections
-APP_STL        := none
-
-ifneq ($(NDK_DEBUG),1)
-APP_CFLAGS     += -Ofast -flto -Werror
-APP_CFLAGS     += -fvisibility=hidden -fvisibility-inlines-hidden
-APP_CFLAGS     += -fno-unwind-tables -fno-asynchronous-unwind-tables
-APP_LDFLAGS    += -flto -Wl,--exclude-libs,ALL -Wl,--strip-all
-endif
+APP_ABI := arm64-v8a
+APP_PLATFORM := android-27
+APP_STL := c++_static
+APP_CPPFLAGS := -std=c++17
