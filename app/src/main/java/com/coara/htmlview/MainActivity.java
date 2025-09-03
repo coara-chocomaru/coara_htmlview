0c4284a7d615a15ac9a59b0a1f36a21f41f7cd6e
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-09-03 11:19:21 +0900

    Update MainActivity.java

--- app/src/main/java/com/coara/htmlview/MainActivity.java
+++ app/src/main/java/com/coara/htmlview/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding.htmlEditText.setLayerType(View.LAYER_TYPE_HARDWARE, null);
         binding.htmlEditText.setMovementMethod(new ScrollingMovementMethod());
-        binding.htmlEditText.setTextIsSelectable(true); // 追加: テキスト選択を有効化（長押しでコピー&ペースト可能に）
+        binding.htmlEditText.setTextIsSelectable(true);
         binding.htmlEditText.setKeyListener(null);
 
         binding.loadButton.setOnClickListener(v -> {
